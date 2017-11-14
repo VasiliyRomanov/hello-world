f3e17f605e3e6fb4c9372a74bf14acedf95c2a23
VasiliyRomanov <31405533+VasiliyRomanov@users.noreply.github.com>
2017-11-14 22:58:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,179 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import update from 'immutability-helper';
 
 
 class SalaryRow extends React.Component {
 
+  render() {    
+    var employeeID = this.props.addEmployee.id;
 
+    var salaryHandler = this.props.salary;
+    var changeSalary = function (s) { salaryHandler(employeeID, s.target.value);};
 
-  constructor() {
-    super();
-    this.state = {value: ''};
-    this.salaryChange = this.salaryChange.bind(this);
-  }
+    var idHandler = this.props.id;
+    var changeID = function (i) { idHandler(employeeID, i.target.value);};
 
- salaryChange(event) {
-    this.setState({value: event.target.value});
-  }
+    var nameHandler = this.props.name;
+    var changeName = function (n) { nameHandler(employeeID, n.target.value);};
 
-  render() {
+    var depHandler = this.props.dep;
+    var changeDep = function (d) { depHandler(employeeID, d.target.value);};
+
+    var deleteData = this.props.del;
+    var delHandler = function() {deleteData(employeeID);};
+
+    var fireEmployee = this.props.fire;
+    var fireHandler = function() {fireEmployee(employeeID);};
+
+    var isID = this.props.addEmployee.id === '' ? <input type="text" onBlur={changeID} /> : this.props.addEmployee.id;
+    var isName = this.props.addEmployee.name === '' ? <input type="text" onBlur={changeName} /> : this.props.addEmployee.name;
+    var isDep = this.props.addEmployee.dep === '' ? <input type="text" onBlur={changeDep} /> : this.props.addEmployee.dep;
+    var isSalary = this.props.addEmployee.salary === '' ? <input type="number" onBlur={changeSalary} /> : this.props.addEmployee.salary;
     
     return (
       <tr>
-        <td>{this.props.addEmployee.id}</td>
-        <td>{this.props.addEmployee.name}</td>
-        <td>{this.props.addEmployee.dep}</td>
-        <td><input type="number" onChange={this.salaryChange} /></td>
-        <td>{this.state.value*0.13}</td>
+        <td>{isID}</td>
+        <td>{isName}</td>
+        <td>{isDep}</td>
+        <td>{isSalary}</td>
+        <td>{this.props.addEmployee.ndfl}</td>
+        <td><button onClick={delHandler} /></td>
+        <td><button onClick={fireHandler}>Удалить</button></td>
       </tr>
     );
   }
-}
+};
 
 class SalaryTable extends React.Component {
-  render() {
-    var rows = [];
-    this.props.employee.forEach(function(addEmployee) {
-      rows.push(<SalaryRow addEmployee={addEmployee} key={addEmployee.id} />);
+
+constructor() {
+      super();
+
+      this.updateID = this.updateID.bind(this);
+      this.updateName = this.updateName.bind(this);      
+      this.updateDep = this.updateDep.bind(this);
+      this.updateSalary = this.updateSalary.bind(this);
+      this.addNewRow = this.addNewRow.bind(this);
+      this.deleteEmployeeData = this.deleteEmployeeData.bind(this);
+      this.fireEmployee = this.fireEmployee.bind(this);
+
+      this.state = {
+        employees: [
+              {id: 1, name: 'Иван Пупкин', dep: 'Администрация', salary: '', ndfl: ''},
+              {id: 2, name: 'Петр Гаврилкин', dep: 'IT-отдел', salary: '', ndfl: ''},
+              {id: 3, name: 'Анна Смирнова', dep: 'Бухгалтерия', salary: '', ndfl: ''},
+              {id: 4, name: 'Сергей Чашкин', dep: 'Снабжение', salary: '', ndfl: ''}
+            ]
+      };
+   }
+
+   addNewRow() {
+    var employees = this.state.employees;
+    var newEmployees = update(employees, {$push: [{id: '', name: '', dep: '', salary: '', ndfl: ''}]});
+    this.setState({
+      employees: newEmployees 
     });
+   }
+
+   updateSalary(id, salary) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var updatedSalary = update(employees[index], {salary: {$set: salary}, ndfl: {$set: salary*0.13}});
+
+    var newSalary = update(employees, {
+      $splice: [[index, 1, updatedSalary]]
+    });
+    this.setState({
+      employees: newSalary 
+    });
+   }
+
+   updateID(id, inputID) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var updatedID = update(employees[index], {id: {$set: inputID}});
+
+    var newID = update(employees, {
+      $splice: [[index, 1, updatedID]]
+    });
+    this.setState({
+      employees: newID 
+    });
+   }
+
+   updateName(id, name) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var updatedName = update(employees[index], {name: {$set: name}});
+
+    var newName = update(employees, {
+      $splice: [[index, 1, updatedName]]
+    });
+    this.setState({
+      employees: newName
+    });
+   }
+
+   updateDep(id, dep) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var updatedDep = update(employees[index], {dep: {$set: dep}});
+
+    var newDep = update(employees, {
+      $splice: [[index, 1, updatedDep]]
+    });
+    this.setState({
+      employees: newDep
+    });
+   }
+
+   deleteEmployeeData(id) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var deleteData = update(employees[index], {id: {$set: ''}, name: {$set: ''}, dep: {$set: ''}, salary: {$set: ''}, ndfl: {$set: ''}});
+
+    var deleteInfo = update(employees, {
+      $splice: [[index, 1, deleteData]]
+    });
+
+    this.setState({
+      employees: deleteInfo 
+    });
+   }
+
+
+   fireEmployee(id) {
+    var employees = this.state.employees;
+    var index = employees.findIndex(function(c) {
+      return c.id === id;
+    });
+
+    var fire = update(employees, {
+      $splice: [[index, 1]]
+    });
+
+    this.setState({
+      employees: fire 
+    });
+   }
+
+  render() {
     return (
       <table>
         <thead>
@@ -50,10 +186,9 @@ class SalaryTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-        	{rows}
-        	<tr>
-        		<td colspan="5">Total:</td>
-        	</tr>
+            {this.state.employees.map((dynamicComponent) => <SalaryRow 
+            key = {this.state.employees.id} addEmployee = {dynamicComponent} salary={this.updateSalary} id={this.updateID} name={this.updateName} dep={this.updateDep} del={this.deleteEmployeeData} fire={this.fireEmployee} />)}
+            <tr><td><button onClick={this.addNewRow}>Добавить</button></td></tr>
         </tbody>
       </table>
     );
@@ -61,25 +196,7 @@ class SalaryTable extends React.Component {
 }
 
 
-class FilterableSalaryTable extends React.Component {
-  render() {
-    return (
-      <div>
-        <SalaryTable employee={this.props.employee} />
-      </div>
-    );
-  }
-}
-
-
-var employees = [
-  {id: 1, name: 'Иван Пупкин', dep: 'Администрация', salary: '', ndfl: ''},
-  {id: 2, name: 'Петр Гаврилкин', dep: 'IT-отдел', salary: '', ndfl: ''},
-  {id: 3, name: 'Анна Смирнова', dep: 'Бухгалтерия', salary: '', ndfl: ''},
-  {id: 4, name: 'Сергей Чашкин', dep: 'Снабжение', salary: '', ndfl: ''}
-];
- 
 ReactDOM.render(
-  <FilterableSalaryTable employee={employees} />,
+  <SalaryTable />,
   document.getElementById('root')
 );
